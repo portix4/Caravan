8d2b1ff7d7665556120de78ce919b88628ead92f
Pablo Portillo <paabloportillo@gmail.com>
2024-03-06 19:40:48 +0100

    rates

--- src/pages/Rates/Rates.jsx
+++ src/pages/Rates/Rates.jsx
@@ -2,10 +2,18 @@ import { Container } from 'react-bootstrap'
 import './Rates.css'
 
 const Rates = () => {
+  const RATES = [
+    {
+      name: 'Temporada Alta',
+      description: 'Fechas de verano (Julio - Agosto)',
+      rate: '150€',
+      URL: 'https://portix4.github.io/IH-Project1-Game2D/'
+    }
+  ]
   return (
     <div className='rates-container'>
       <Container>
-        <h2>Soy rates</h2>
+        <h2>Consulta nuestras tarifas</h2>
       </Container>
     </div>
   )
