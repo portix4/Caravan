ba32f668fe3e5f7ec4216866ab150ec2ee68320e
Pablo Portillo <paabloportillo@gmail.com>
2024-04-16 14:32:29 +0200

    brevo

--- src/pages/Form/FormPage.jsx
+++ src/pages/Form/FormPage.jsx
@@ -1,23 +1,86 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { Form, Button } from 'react-bootstrap'
 const FormPage = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    textContext: ''
+  })
+
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    try {
+      const response = await axios.post('http://localhost:5005/api', formData)
+      console.log('Formulario enviado:', response.data)
+      // Aquí podrías añadir más lógica, como limpiar el formulario o mostrar un mensaje de éxito/error
+    } catch (error) {
+      console.error('Error al enviar formulario:', error)
+    }
+  }
+
   return (
-    <div>
-      <iframe
-        title='Custom Content'
-        width='540'
-        height='305'
-        src='https://a275efc3.sibforms.com/serve/MUIFAACUE91Xfxy8K7Zwu8oBLNDR3ZP1_bZNUxtrMAe4-4NYW1E_lWxDDkednX5Y1mnKnkJSD9hiTqVm7Q7snx89SBXhz0NJF3jnVozMlJK3SrPx6voMAeHvJwA-_4xeMcTrqn9ZMr_Ockpyy4ti2wUPUlrooc3tbbiQC4vKu8Dum2qFrY5UHK41I-ADgrC-gnJPXCPlm6_ysTlw'
-        frameBorder='0'
-        scrolling='auto'
-        allowFullScreen
-        style={{
-          display: 'block',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          maxWidth: '100%'
-        }}
-      ></iframe>
-      <p>https://developers.brevo.com/</p>
+    <div className='InfoForm'>
+      <Form onSubmit={handleSubmit} autoComplete='off'>
+        <Form.Group className='mb-3 emailText' controlId='email'>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            className='loginControl'
+            type='email'
+            value={formData.email}
+            onChange={handleChange}
+            name='email'
+          />
+        </Form.Group>
+        <Form.Group className='mb-3 passwordText' controlId='password'>
+          <Form.Label>Nombre</Form.Label>
+          <Form.Control
+            className='loginControl'
+            type='text'
+            value={formData.name}
+            onChange={handleChange}
+            name='name'
+          />
+        </Form.Group>
+        <Form.Group className='mb-3 descriptionText' controlId='description'>
+          <Form.Label>Descripción</Form.Label>
+          <Form.Control
+            className='loginControl'
+            as='textarea'
+            rows={3}
+            value={formData.textContext}
+            onChange={handleChange}
+            name='textContext'
+          />
+        </Form.Group>
+        <Button variant='primary' type='submit'>
+          Submit{' '}
+        </Button>
+      </Form>
     </div>
+
+    // {/* <Form>
+    //   <Form.Group className='mb-3' controlId='formBasicEmail'>
+    //     <Form.Label>Email address</Form.Label>
+    //     <Form.Control type='email' placeholder='Enter email' />
+    //     <Form.Text className='text-muted'>
+    //       We'll never share your email with anyone else.
+    //     </Form.Text>
+    //   </Form.Group>
+
+    //   <Form.Group className='mb-3' controlId='formBasicPassword'>
+    //     <Form.Label>Nombre</Form.Label>
+    //     <Form.Control type='text' placeholder='Name' />
+    //   </Form.Group>
+    //   <Form.Group className='mb-3' controlId='formBasicCheckbox'></Form.Group>
+    //   <Button variant='primary' type='submit'>
+    //     Submit
+    //   </Button>
+    // </Form> */}
   )
 }
 
